671954e3f3753ebbbc9c1c145aa443c44a3a0a0a
faouzi892 <f55688119@gmail.com>
2024-06-02 19:49:57 +0100

    cat-amazon

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const express = require("express");
 const { scrapeLogic } = require("./scrapeLogic");
+
+const { cat_amazon } = require("./cat-amazon");
 const app = express();
 
 const PORT = process.env.PORT || 4000;
 
 app.get("/scrape", (req, res) => {
   scrapeLogic(res);
+});
+
+app.get("/cat-amazon", (req, res) => {
+  cat_amazon(req, res);
 });
 
 app.get("/", (req, res) => {
