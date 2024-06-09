ab45b04292c85f92542d2884ed9ab4f40762c25a
faouzi892 <f55688119@gmail.com>
2024-06-09 19:50:51 +0100

    headers-76

--- cat-amazon.js
+++ cat-amazon.js
@@ -22,7 +22,6 @@ try {
     const page = await browser.newPage();
     const headers = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/53",
-        "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:125.0) Gecko/20100101 Firefox/125.0",
         "Accept": "text/html,*/*",
         "Accept-Language": "en-US,en;q=0.5",
         "Accept-Encoding": "gzip, deflate, br",
