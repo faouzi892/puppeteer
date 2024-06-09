03c19e83e6b740510a4b6dbf8ffd41b8ef2898a8
faouzi892 <f55688119@gmail.com>
2024-06-09 17:08:25 +0100

    headers-1

--- cat-amazon.js
+++ cat-amazon.js
@@ -28,7 +28,7 @@ try {
       "Connection": "keep-alive",
       "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:125.0) Gecko/20100101 Firefox/125.0",
       "Referer": "https://www.amazon.com/",
-      "Alt-Used": "www.amazon.com",
+      
       
   }
     await page.setExtraHTTPHeaders(headers);
