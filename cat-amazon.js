ab03557e46547bf7dc14b0c5c8316b300708fb24
faouzi892 <f55688119@gmail.com>
2024-06-09 17:50:33 +0100

    headers-43

--- cat-amazon.js
+++ cat-amazon.js
@@ -22,8 +22,8 @@ try {
     const page = await browser.newPage();
     const headers = {
       
-       "Accept-Language": "en-US,en;q=0.5",
-     
+      "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
+      "Accept-Language": "en-US,en;q=0.5",
       "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:125.0) Gecko/20100101 Firefox/125.0",
      
       
