43ab95f0e840ce6ab333e75835f0b2c969ae5172
faouzi892 <f55688119@gmail.com>
2024-06-09 20:15:37 +0100

    headers-87

--- cat-amazon.js
+++ cat-amazon.js
@@ -21,13 +21,19 @@ try {
     
     const page = await browser.newPage();
     const headers = {
-        "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:125.0) Gecko/20100101 Firefox/125.0",
-        "Accept": "text/html,*/*",
-        "Accept-Language": "en-US,en;q=0.5",
-        "Accept-Encoding": "gzip, deflate, br",
-        "Alt-Used": "www.amazon.com",
-        "Connection": "keep-alive",
-        "Referer": "https://www.amazon.com/"
+        "Accept" : "text/html,*/*",
+        "Accept-Encoding" : "gzip, deflate, br",
+        "Accept-Language" : "en-US,en;q=0.5",
+        "Alt-Used" : "www.amazon.com",
+        "Connection" : "keep-alive",
+        "Host" : "www.amazon.com",
+        "Referer" : "https://www.amazon.com",
+        "Sec-Fetch-Dest" : "empty",
+        "Sec-Fetch-Mode" : "cors",
+        "Sec-Fetch-Site" : "same-origin",
+        "TE" : "trailers",
+        "User-Agent" : "Mozilla/5.0 (X11; Linux x86_64; rv:125.0) Gecko/20100101 Firefox/125.0",
+        "X-Requested-With" : "XMLHttpRequest",
       
   }
     await page.setExtraHTTPHeaders(headers);
