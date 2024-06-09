83b9d3a9543025b7ada57e8acea683c7487d2eeb
faouzi892 <f55688119@gmail.com>
2024-06-09 18:26:35 +0100

    headers-65

--- cat-amazon.js
+++ cat-amazon.js
@@ -24,7 +24,7 @@ try {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/53"
           
   }
-    await page.setExtraHTTPHeaders(headers);
+    //await page.setExtraHTTPHeaders(headers);
     await page.setViewport({ width: 1280, height: 800 });
 
     let allData = [];
