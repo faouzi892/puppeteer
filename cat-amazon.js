f820380aaa9baec78f5d21becfeb9eb6b63ff392
faouzi892 <f55688119@gmail.com>
2024-06-09 17:57:02 +0100

    headers-4

--- cat-amazon.js
+++ cat-amazon.js
@@ -25,7 +25,7 @@ try {
       "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
       "Accept-Language": "en-US,en;q=0.5",
       "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:125.0) Gecko/20100101 Firefox/125.0",
-     
+      "Referer": "https://www.amazon.com/"
       
   }
     await page.setExtraHTTPHeaders(headers);
