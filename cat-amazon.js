e1d1c8635968bff1a7be6b75ad226c9b66c5376e
faouzi892 <f55688119@gmail.com>
2024-06-09 17:38:45 +0100

    headers-3

--- cat-amazon.js
+++ cat-amazon.js
@@ -22,12 +22,10 @@ try {
     const page = await browser.newPage();
     const headers = {
       
-      "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
-      "Accept-Encoding": "gzip, deflate, br",
-      "Accept-Language": "en-US,en;q=0.5",
-      "Connection": "keep-alive",
+       "Accept-Language": "en-US,en;q=0.5",
+     
       "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:125.0) Gecko/20100101 Firefox/125.0",
-      "TE": "trailers"
+     
       
   }
     await page.setExtraHTTPHeaders(headers);
